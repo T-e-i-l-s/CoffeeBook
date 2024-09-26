c21afe625a916c15b0ab45c3744cb99a98ab99dc
Karim <kfmust@gmail.com>
2024-09-26 17:04:09 +0300

    Init

---
+++ app/src/main/java/com/mustafin/ebooks/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.mustafin.ebooks
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+        }
+    }
+}
